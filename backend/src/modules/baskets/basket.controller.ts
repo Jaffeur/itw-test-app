b0214c1bc30bc5339bf69935193bfdfffda0f66f
RATOVONJANAHARY Miandry Fanomezantsoa <ratovonjanahary.miandryfano@gmail.com>
2022-03-12 11:52:26 +0300

    Endpoints for handling adding or removing dish to/from the current basket

--- backend/src/modules/baskets/basket.controller.ts
+++ backend/src/modules/baskets/basket.controller.ts
@@ -1,9 +1,9 @@
 import { EntityManager } from '@mikro-orm/core';
-import { Controller, Get, Param, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, UseInterceptors } from '@nestjs/common';
 
-import { BasketEntity, BasketDishEntity } from '../../database/entities';
+import { BasketEntity, BasketDishEntity, DishEntity } from '../../database/entities';
 
-import { BasketParams } from './dto/basket.dto';
+import { AddBasketDishDTO, BasketParams, DeleteBasketDishDTO } from './dto/basket.dto';
 import { PgQueryErrorInterceptor } from '../interceptors';
 
 @Controller('baskets')
@@ -55,6 +55,87 @@ export class BasketController {
     return {
       ...basket,
       dishes: basketDishes.map((basketDish) => basketDish.dish),
+    };
+  }
+
+  /**
+   * Adding a dish to the current basket
+   * @param param0 Uuid of a dish object
+   * @returns 201 Http response
+   */
+  @HttpCode(201)
+  @Post('current/dishes')
+  async addDish(@Body() body: AddBasketDishDTO) {
+    // Get the current basket
+    const basket = await this.entityManager.findOne(BasketEntity, { payed: false });
+
+    // Get the dish object matching the given id
+    const dish = await this.entityManager.findOne(DishEntity, { id: body.id });
+
+    // Get the quantity of dish
+    const quantity = body.quantity;
+
+    // Get existing dishes within the basket
+    const existing_dishes = await this.entityManager.find(
+      BasketDishEntity,
+      { basket: basket.id },
+      { populate: ['dish'] },
+    );
+
+    // Attach dish to the current basket by adding row in the basket_dish bridge table
+    const basket_dish = new BasketDishEntity({ basket, dish });
+    basket_dish.quantity = quantity;
+
+    // Add new dish to existing dishes and then persist them
+    existing_dishes.push(basket_dish);
+    await this.entityManager.persistAndFlush(existing_dishes);
+
+    // Return the current basket along with its selected dishes
+    return {
+      ...basket,
+      dishes: existing_dishes.map((basket_dish) => ({
+        dish: basket_dish.dish,
+        quantity: basket_dish.quantity,
+      })),
+    };
+  }
+
+  /**
+   * Adding a dish to the current basket
+   * @param param0 Uuid of a dish object
+   * @returns 201 Http response
+   */
+  @Delete('current/dishes')
+  async deleteDish(@Body() body: DeleteBasketDishDTO) {
+    // Get the current basket
+    const basket = await this.entityManager.findOne(BasketEntity, { payed: false });
+
+    // Get the dish object matching the given id
+    const dish = await this.entityManager.findOne(DishEntity, { id: body.id });
+
+    // Get existing dishes
+    const basket_dish = await this.entityManager.find(BasketDishEntity, {
+      basket: basket.id,
+      dish: dish.id,
+    });
+
+    // Remove the dish from the basket by removing the basket_dish item from database
+    await this.entityManager.removeAndFlush(basket_dish);
+
+    // Get existing dishes
+    const existing_dishes = await this.entityManager.find(
+      BasketDishEntity,
+      { basket: basket.id },
+      { populate: ['dish'] },
+    );
+
+    // Return the current basket along with its selected dishes
+    return {
+      ...basket,
+      dishes: existing_dishes.map((basket_dish) => ({
+        dish: basket_dish.dish,
+        quantity: basket_dish.quantity,
+      })),
     };
   }
 }
--- backend/src/modules/baskets/dto/basket.dto.ts
+++ backend/src/modules/baskets/dto/basket.dto.ts
@@ -1,16 +1,34 @@
 import { applyDecorators } from '@nestjs/common';
+import { Transform } from 'class-transformer';
 import {
   ArrayUnique,
   IsArray,
   IsEnum,
+  IsInt,
   IsNotIn,
   IsNumber,
   IsOptional,
+  IsPositive,
   IsString,
   IsUUID,
 } from 'class-validator';
 
 export class BasketParams {
+  @IsUUID(4)
+  id: string;
+}
+
+export class AddBasketDishDTO {
+  @IsUUID(4)
+  id: string;
+
+  @Transform(({ value }) => parseInt(value))
+  @IsInt()
+  @IsPositive()
+  quantity: number;
+}
+
+export class DeleteBasketDishDTO {
   @IsUUID(4)
   id: string;
 }
